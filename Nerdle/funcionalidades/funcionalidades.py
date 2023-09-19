3835a7a402902b1d4990eb448c66600631bcc685
hola0511 <espinosaariassamuel@gmail.com>
2023-09-18 23:12:43 -0500

    parte basica del uml

---
+++ Nerdle/funcionalidades/funcionalidades.py
@@ -0,0 +1,30 @@
+class Jugador:
+
+    def registrar_jugador(self):
+        pass
+    def crear_jugador(self):
+        pass
+
+
+class problema:
+
+    def crear_problema(self):
+        pass
+
+    def mostrar_problema(self):
+        pass
+
+
+class juego:
+
+    def cantidad_intentos(self):
+        pass
+
+    def comparar_problemas(self):
+        pass
+
+    def finalizar_problema(self):
+        pass
+
+    def siguiente_intento(self):
+        pass
