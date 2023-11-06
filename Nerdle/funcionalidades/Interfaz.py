b5bcf5605dd1f3ac32b1b5bcadf078a68ec7f5eb
hola0511 <espinosaariassamuel@gmail.com>
2023-11-06 01:41:31 -0500

    funcion del boton guardar_intento añadida y agregado el display de intentos

--- Nerdle/funcionalidades/Interfaz.py
+++ Nerdle/funcionalidades/Interfaz.py
@@ -1,23 +1,28 @@
 import tkinter as tk
-#from funcionalidades import comparar_problemas  , command=comparar_problemas
+import funcionalidades
+
 ventana = tk.Tk()
 ventana.title("Nerdle")
 ventana.geometry("600x400")
 
-bienvenida = tk.Label(ventana, text = "Bienbenido a Nerdle")
+bienvenida = tk.Label(ventana, text="Bienbenido a Nerdle")
 bienvenida.pack()
-reglas = tk.Label(ventana, text = "tienes 6 intetos para adivinar la secuencia de numeros la cual esta formada por 8 elementos")
-reglas1 = tk.Label(ventana, text = "entre los cuales pueden estas incluidos numeros del '0' al '9' y operadores")
-reglas2 = tk.Label(ventana, text = "tales como '+', '-', '*', '/','='")
+
+reglas = tk.Label(ventana, text="tienes 6 intetos para adivinar la secuencia de numeros la cual esta formada por 8 elementos")
+reglas1 = tk.Label(ventana, text="entre los cuales pueden estas incluidos numeros del '0' al '9' y operadores")
+reglas2 = tk.Label(ventana, text="tales como '+', '-', '*', '/','='")
 reglas.pack()
 reglas1.pack()
 reglas2.pack()
 
-intento = tk.Entry()
-intento.pack()
+intento_pantalla = tk.Entry()
+intento_pantalla.pack()
 
-guess = tk.Button(text="guardar intento")
-guess.pack()
+intentos = tk.Label(ventana, text = funcionalidades.Juego.cantidad_intentos)
+
+
+guardar_intento = tk.Button(text="guardar intento", command=funcionalidades.Juego.comparar_problemas)
+guardar_intento.pack()
 
 
 ventana.mainloop()
