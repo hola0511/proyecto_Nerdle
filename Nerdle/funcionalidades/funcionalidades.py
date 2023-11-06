e460da2d860bdc2312fef2da94e6253445eb3881
hola0511 <espinosaariassamuel@gmail.com>
2023-11-06 01:42:44 -0500

    class juego avanzada

--- Nerdle/funcionalidades/funcionalidades.py
+++ Nerdle/funcionalidades/funcionalidades.py
@@ -1,4 +1,5 @@
 import random
+import Interfaz
 
 
 class Jugador:
@@ -20,16 +21,31 @@ class problema:
         pass
 
 
-class juego:
+class Juego:
+
+    def __init__(self):
+        self.intentos_maximos = 6
+        self.intentos_restantes = self.intentos_maximos
+        self.problem = problema()
+        self.target_sequence = self.problem.crear_problema()
+
 
     def cantidad_intentos(self):
-        pass
+        return self.intentos_restantes
 
-    def comparar_problemas(self):
-        pass
-
-    def finalizar_problema(self):
-        pass
+    def comparar_problemas(self, intento):
+        intento = intento_pantalla.get()
+        correct_position = 0
+        correct_elements = 0
+        incorrect_elements = 0
+        for i in range(8):
+            if intento[i] == self.target_sequence[i]:
+                correct_position += 1
+            elif intento[i] in self.target_sequence:
+                correct_elements += 1
+            else:
+                incorrect_elements += 1
+        return correct_position, correct_elements, incorrect_elements
 
     def siguiente_intento(self):
         pass
